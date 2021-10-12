a5ffc3548e4e26f3c1a754cf36e67d3bb5be0e41
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-10-11 21:13:17 -0300

    09_dates.php

--- 09_dates.php
+++ 09_dates.php
@@ -1,16 +1,34 @@
 <?php
 
-// Print current timestamp
+// // Print current timestamp
+// echo time().'<br>';
 
-// Print current date
+// // Print current date
+// echo date('Y-m-d H:i:s').'<br>';
 
-// Print yesterday
+// // Print yesterday
+// echo date('Y-m-d H:i:s', time() - 60 * 60 * 24).'<br>';
 
-// Different format: https://www.php.net/manual/en/function.date.php
+// // Different format: https://www.php.net/manual/en/function.date.php
+// echo date('F j Y, H:i:s').'<br>';
 
-// String to timestamp
+// // String to timestamp
+// echo strtotime('+1 week').'<br>';
 
-// Parse date: https://www.php.net/manual/en/function.date-parse.php
+// // Parse date: https://www.php.net/manual/en/function.date-parse.php
+// $dateString = '2020-02-06 12:45:35';
+// $parseDate = date_parse($dateString);
+// echo '<pre>';
+// var_dump($parseDate);
+// // echo $parseDate['day'];
+// echo '</pre>';
 
 // Parse date from format
 // https://www.php.net/manual/en/function.date-parse-from-format.php
+$dateString = 'February 4 2020 12:45:35';
+$parseDate = date_parse_from_format('F j Y H:i:s',$dateString);
+echo '<pre>';
+var_dump($parseDate);
+// echo $parseDate['day'];
+echo '</pre>';
+
