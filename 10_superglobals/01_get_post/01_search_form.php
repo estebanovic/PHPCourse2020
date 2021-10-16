1b0bca4156c29999ceecbfa237f9d6fb5ca909e8
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-10-15 23:55:11 -0300

    10_superglobals\01_get_post\01_search_form.php_3

--- 10_superglobals/01_get_post/01_search_form.php
+++ 10_superglobals/01_get_post/01_search_form.php
@@ -1,25 +1,38 @@
 <?php
+
 /**
  * User: TheCodeholic
  * Date: 2/8/2020
  * Time: 9:49 AM
  */
 
+echo '<pre>';
+var_dump($_GET);
+echo '</pre>';
+
+$keyword = "";
+if (isset($_GET['keyword'])) {
+  $keyword = $_GET['keyword'];
+  echo $keyword . '<br>';
+}
+
 ?>
 <!doctype html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
 </head>
+
 <body>
-<form action="" method="get">
-  <input type="text" name="keyword"
-         placeholder="Type and hit enter">
-  <button>Search</button>
-</form>
+  <form action="<?php ?>" method="get">
+    <input type="text" name="keyword" value="<?php $keyword ?>" placeholder="Type and hit enter">
+    <!-- <input type="text" name="id" placeholder="type id"> -->
+    <button>Search</button>
+  </form>
 </body>
+
 </html>
