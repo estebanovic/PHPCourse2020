064fafcfb76926efcf720a36e908e1e71b6f7f7c
Esteban Obilinovic <eobilinovic@dcanje.com>
2021-10-21 14:41:12 -0300

    10_superglobals\03_file_uploading\file_upload.php

--- 10_superglobals/03_file_uploading/file_upload.php
+++ 10_superglobals/03_file_uploading/file_upload.php
@@ -1,19 +1,42 @@
 <?php
 
+$errorMessage = '';
+
+if (isset($_FILES['file'])) {
+  echo '<pre>';
+  var_dump($_FILES);
+  echo '</pre>';
+
+  $file = $_FILES['file'];
+  $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
+  $ext = strtolower($ext);
+  
+  if ($file['size'] > 5 * 1024 * 1024) {
+    $errorMessage = 'You can not upload more than 5MB files';
+  } else if (!in_array($ext ,['png','dvg','jpg','jpeg'])) {
+    $errorMessage = 'You can only upload images';
+  } else {
+    move_uploaded_file($_FILES['file']['tmp_name'], $_FILES['file']['name']);
+  }
+}
+
 ?>
 <!doctype html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
 </head>
+
 <body>
-<form action="" method="post">
-  <input type="file" name="file"><br>
-  <button>Submit</button>
-</form>
+  <p><?php echo $errorMessage ?></p>
+  <form action="" method="post" enctype="multipart/form-data">
+    <input type="file" name="file"><br>
+    <button>Submit</button>
+  </form>
 </body>
+
 </html>
