9a0659c79d511cf624bd00128dd64d618aa1a230
Esteban Obilinovic <eobilinovic@dcanje.com>
2021-10-22 14:48:07 -0300

    12_fs

--- 12_fs/index.php
+++ 12_fs/index.php
@@ -1,15 +1,29 @@
 <?php
 // Magic constants
+echo __DIR__ . '<br>';
+echo __FILE__ . '<br>';
 
 // Create directory
+// mkdir('test');
 
 // Rename directory
+// rename('test',  'myfolder');
 
 // Delete directory
+// rmdir('myfolder');
 
 // Read files and folders inside directory
+// $files = scandir('./');
+// echo '<pre>';
+// var_dump($files);
+// echo '</pre>';
 
 // file_get_contents, file_put_contents
+// echo file_get_contents('lorem.txt');
+// file_put_contents('test.txt','Hello world');
+
+$content = file_get_contents('lorem.txt');
+file_put_contents('lorem.txt','My text' . PHP_EOL . $content);
 
 // https://www.php.net/manual/en/book.filesystem.php
 // file_exists
