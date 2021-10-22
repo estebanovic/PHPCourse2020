180ea7c71e7ca625e38d0c871192e08e9696320a
Esteban Obilinovic <eobilinovic@dcanje.com>
2021-10-22 16:23:29 -0300

    13_todo_list_with_fs

---
+++ 13_todo_list_with_fs/newtodo.php
@@ -0,0 +1,20 @@
+<?php
+// echo '<pre>';
+// var_dump($_POST);
+// echo '</pre>';
+
+$todoname = $_POST['todo_name'] ?? '';
+$todoname = trim($todoname);
+
+if($todoname){
+    if (file_exists('todo.json')){
+        $json = file_get_contents('todo.json');
+        $jsonArray = json_decode($json, true);
+    } else {
+        $jsonArray = [];
+    }
+    $jsonArray[$todoname] = ['completed' => false];
+    file_put_contents('todo.json',json_encode($jsonArray, JSON_PRETTY_PRINT));
+}
+
+header('Location: index.php');
