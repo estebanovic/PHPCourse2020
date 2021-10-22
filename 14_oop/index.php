0d51428dd117d26e6e9189d79fb90b728b5a59c0
Esteban Obilinovic <eobilinovic@dcanje.com>
2021-10-22 18:00:54 -0300

    14_oop

--- 14_oop/index.php
+++ 14_oop/index.php
@@ -2,6 +2,26 @@
 
 // What is class and instance
 
+require_once "./Person.php";
+require_once "./Student.php";
+
+// $p = new Person("Zura", 28, 15000);
+
+// // echo $p.getSalary();
+
+// echo $p -> name . '<br>';
+// echo $p -> age . '<br>';
+// echo $p -> getSalary() . '<br>';
+// $p -> setSalary(20000);
+// echo $p -> getSalary() . '<br>';
+
+// echo '<pre>';
+// var_dump($p);
+// echo '</pre>';
+
+$s = new Student("Carl", 20, 1234);
+echo $s -> name . '<br>';
+
 // Create Person class in Person.php
 
 // Create instance of Person
