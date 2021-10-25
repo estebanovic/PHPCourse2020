b07d3f03139f51b973c56602f1dae0ebe4ffa2a9
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-10-24 23:10:23 -0300

    16_autoloading

--- 16_autoloading/index.php
+++ 16_autoloading/index.php
@@ -1,1 +1,24 @@
 <?php
+// require_once './app/Email.php';
+// require_once './app/Person.php';
+require_once "./vendor/autoload.php";
+
+// use app\Email;
+// use app\Person;
+
+// $email = new Email();
+// $email = new Email();
+// $email = new Email();
+
+// $person = new Person();
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('output.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
